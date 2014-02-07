c647ff5bbf2c3034deeb0e0b459c2c4b9e9d0844
spby79 <spby79@gmail.com>
2014-02-07 22:33:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot2::Application.config.secret_key_base = '624d88c6a53a054754168b85afa1fd5a38c6bd6f9063f8dde8bec9135073693025cf9fddf5c24ad941e25ea13b7afe069bff45b8bb147f13a480fa850c5e0ae2'
---
+++ db/migrate/20140207011848_combine_items_in_cart.rb
@@ -0,0 +1,17 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  
+  def down
+    # split items with quantity>1 into multiple items
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do 
+        LineItem.create cart_id: line_item.cart_id,
+          product_id: line_item.product_id, quantity: 1
+      end
+ 
+      # remove original item
+      line_item.destroy
+    end
+  end
+end
+
